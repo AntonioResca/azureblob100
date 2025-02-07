01568cf219199179a5799628b73849c59a111f78
AntonioResca <antonio.resca@gmail.com>
2025-02-07 10:51:14 +0100

    added function read_blob

--- function_app.py
+++ function_app.py
@@ -25,3 +25,9 @@ def write_blob(req: func.HttpRequest) -> func.HttpResponse:
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
+
+@app.route(route="read_blob")
+def read_blob(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    return func.HttpResponse(f"Hello, {blob.read_blob_from_storage()}. I read your name in Azure Storage")
