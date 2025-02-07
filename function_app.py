76974866555771ca0651eb46a2f12edc28f7050e
AntonioResca <antonio.resca@gmail.com>
2025-02-07 10:45:29 +0100

    renamed function http_trigger to write_blob

--- function_app.py
+++ function_app.py
@@ -4,8 +4,8 @@ from blobfun import blob as blob
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
-@app.route(route="http_trigger")
-def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
+@app.route(route="write_blob")
+def write_blob(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     name = req.params.get('name')
