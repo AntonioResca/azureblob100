5a67362355687f123ada50d7ae2f3fe2a805c21a
AntonioResca <antonio.resca@gmail.com>
2025-02-26 15:04:52 +0100

    more csv and json examples

--- blobfun/blob.py
+++ blobfun/blob.py
@@ -1,6 +1,8 @@
 import os
+import io
 import json
 import csv
+import pandas as pd 
 import logging
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 
@@ -64,6 +66,99 @@ def write_json_to_storage(mydict):
     except Exception as e:
         logging.error(f"Error uploading blob: {e}")
         return str(e)
+    
+
+def write_csv_to_storage(mydict):
+    try:
+        # Retrieve connection string from environment variable
+        connection_string = os.environ["AzureWebJobsStorage"]
+        
+        # Initialize BlobServiceClient
+        blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+        
+        # Define container name and blob name
+        container_name = "mycontainer"
+        blob_name = "example.csv"
+        
+        # Create container if it doesn't exist
+        container_client = blob_service_client.get_container_client(container_name)
+        if not container_client.exists():
+            container_client.create_container()
+
+        output = io.StringIO()
+        writer = csv.DictWriter(output, fieldnames=mydict[0].keys())
+        writer.writeheader()
+        writer.writerows(mydict)
+        
+        # Ottieni i dati CSV come stringa
+        csv_data = output.getvalue()
+        # Upload json to blob
+        blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+        blob_client.upload_blob(csv_data, overwrite=True)
+
+        logging.info(f"Blob '{blob_name}' uploaded successfully.")
+        return f"Blob '{blob_name}' uploaded successfully."
+
+    except Exception as e:
+        logging.error(f"Error uploading blob: {e}")
+        return str(e)
+
+
+def read_json_from_storage():
+    try:
+        # Retrieve connection string from environment variable
+        connection_string = os.environ["AzureWebJobsStorage"]
+        
+        # Initialize BlobServiceClient
+        blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+        
+        # Define container name and blob name
+        container_name = "mycontainer"
+        blob_name = "example.json"
+
+        # Get the blob client
+        blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+
+        # Check if the blob exists
+        if blob_client.exists():
+            json_data = blob_client.download_blob().content_as_text() 
+            logging.info(f"Blob '{blob_name}' read successfully.")
+            return  json.loads(json_data)
+        else:
+            logging.warning(f"Blob '{blob_name}' does not exist.")
+            return f"Blob '{blob_name}' does not exist."
+
+    except Exception as e:
+        logging.error(f"Error reading blob: {e}")
+        return str(e)
+
+def read_csv_from_storage():
+    try:
+        # Retrieve connection string from environment variable
+        connection_string = os.environ["AzureWebJobsStorage"]
+        
+        # Initialize BlobServiceClient
+        blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+        
+        # Define container name and blob name
+        container_name = "mycontainer"
+        blob_name = "example.csv"
+
+        # Get the blob client
+        blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+        # Check if the blob exists
+        if blob_client.exists():
+            csv_data = blob_client.download_blob().content_as_text()
+            df = pd.read_csv(io.StringIO(csv_data))
+            return df.to_dict(orient='records')
+        else:
+            logging.warning(f"Blob '{blob_name}' read failed ")
+            return f"Blob '{blob_name}' does not exist."
+
+    except Exception as e:
+        logging.error(f"Error reading blob: {e}")
+        return str(e)
+    
 
 def read_blob_from_storage():
     try:
--- function_app.py
+++ function_app.py
@@ -1,6 +1,9 @@
 import azure.functions as func
 import logging
 from blobfun import blob as blob
+import json
+import csv
+import io
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
@@ -57,8 +60,63 @@ def write_json(req: func.HttpRequest) -> func.HttpResponse:
             status_code=200
     )
 
+@app.route(route="write_csv")
+def write_csv(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    myData = [
+        {"nome": "Luigi", "cognome": "Mazzini", "citta": "Napoli"},
+        {"nome": "Marinella", "cognome": "Selva", "citta": "Torino"},
+        {"nome": "Ciro", "cognome": "Iaccarino", "citta": "Napoli"},
+        {"nome": "Claudio", "cognome": "Cibrabrio", "citta": "Ancona"}
+    ]
+    blob.write_csv_to_storage(myData)
+    return func.HttpResponse(
+            "Success!",
+            status_code=200
+    )
+
 @app.route(route="read_blob")
 def read_blob(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     return func.HttpResponse(f"Hello, {blob.read_blob_from_storage()}. I read your name in Azure Storage")
+
+
+@app.route(route="read_json")
+def read_json(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    json_data = json.dumps(blob.read_json_from_storage())
+    
+    # Crea la response HTTP con il tipo di contenuto "application/json"
+    return func.HttpResponse(
+        body=json_data,
+        status_code=200,
+        mimetype="application/json",
+        charset="utf-8"
+    )
+
+@app.route(route="read_csv")
+def read_csv(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    csv_data = blob.read_csv_from_storage()
+
+    output = io.StringIO()
+    writer = csv.DictWriter(output, fieldnames=csv_data[0].keys())
+    
+    writer.writeheader()
+    writer.writerows(csv_data)
+    csv_content = output.getvalue()
+    
+    # Crea la response HTTP con il tipo di contenuto "application/json"
+    return func.HttpResponse(
+        csv_content,
+        status_code=200,
+        mimetype="text/csv",
+        charset="utf-8",
+        headers={
+            "Content-Disposition": 'attachment; filename="data.csv"'
+        }
+    )
